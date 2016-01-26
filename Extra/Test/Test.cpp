28d18bec3afcb87e09885ff72034bd71b518235f
Александр Матузов <a.matuzov@gmail.com>
2016-01-26 22:51:07 +0300

    Test

--- Extra/Test/Test.cpp
+++ Extra/Test/Test.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-
 using namespace std;
+
+/*
+Тестируем кодировку
+*/
 
 void main() {
 	setlocale(LC_CTYPE, "Rus");
 
-	int k = 3;
-	double mas[k] = { 1, 2, 3 };
-	for (int i = 0; i<k; i++)
-		mas[i++] += 4;
-	for (int i = 0; i<k; i++)
-		cout << mas[i];
+	//Код программы
+
+	
 
 	cout << endl;
 	system("pause");
