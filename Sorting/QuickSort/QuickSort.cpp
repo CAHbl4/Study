8b26c67c9ce51383f3fb2612f67a3ad843ffea46
Александр Матузов <a.matuzov@gmail.com>
2016-03-01 20:59:29 +0300

    Qsort

--- Sorting/QuickSort/QuickSort.cpp
+++ Sorting/QuickSort/QuickSort.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+п»ї#include <iostream>
 
 using namespace std;
+
+//Quick sort
+void qsort(int* arr, int n);
 
 void main() {
 	setlocale(LC_CTYPE, "Rus");
@@ -9,10 +12,34 @@ void main() {
 
 	*/
 
-	//Код программы
-
-
+	//РљРѕРґ РїСЂРѕРіСЂР°РјРјС‹
 
 	cout << endl;
 	system("pause");
+}
+
+void qsort(int* arr, int n)
+{
+	int l = 0, r = n - 1;
+	int m = *(arr + n / 2);
+	int tmp;
+
+	do
+	{
+		while (*(arr + l) < m) l++;
+		while (*(arr + r) > m) r--;
+
+		if (l <= r)
+		{
+			tmp = *(arr + l);
+			*(arr + l) = *(arr + r);
+			*(arr + r) = tmp;
+			l++;
+			r--;
+		}
+	} while (l <= r);
+
+
+	if (r > 0) qsort(arr, r + 1);
+	if (n > l) qsort(arr + l, n - l);
 }
