5cc6177a54d0a1d206296386bb8950ef071f264f
Александр Матузов <a.matuzov@gmail.com>
2016-03-10 00:03:58 +0300

    Test

--- Extra/test/test.cpp
+++ Extra/test/test.cpp
@@ -1,38 +1,19 @@
-﻿#include <iostream>
+﻿#include<iostream>
 using namespace std;
 
+int Summa(int a, int b) {
+	return a + b;
+}
+
+int Mult(int a, int b) {
+	return a * b;
+}
+
+
 int main() {
+	int(*ptr)(int, int) = Summa;
 
-	int a = 767;
-	unsigned char *ptr_c = (unsigned char *)&a;
-
-	cout << "Mem addr: ";
-	for (int i = sizeof(a) - 1; i >= 0; i--)
-	{
-		cout.width(9);
-		cout << hex << (int)ptr_c + i;
-	}
-	cout << endl;
-
-	cout << "Binary  : ";
-	for (int i = sizeof(a) - 1; i >= 0; i--)
-	{
-		cout << " ";
-		for (int j = 7; j >= 0; j--)
-		{
-			cout << (*(ptr_c + i) >> j & 1);
-		}
-	}
-	cout << endl;
-
-	cout << "Decimal : ";
-	for (int i = sizeof(a) - 1; i >= 0; i--)
-	{
-		cout.width(9);
-		cout << dec << (int)*(ptr_c + i);
-	}
-
-	cout << endl;
-	system("pause");
+	cout << sizeof(ptr);
+	cout << sizeof(Summa);
 	return 0;
 }
