74b3ab7ed1caab18b9828d69080c9874c212eb28
Александр Матузов <a.matuzov@gmail.com>
2016-03-15 22:35:45 +0300

    Home1_m8 start

---
+++ Lesson30/Home1_m8/Home1_m8.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+/*
+* Задан текст, состоящий из строк. Строк не менее 10. В строке не менее 5
+* слов. В слове не менее 5 символов. Слова разделены 1 пробелом.
+* Осуществить сортировку строк по количеству в них заданного символа в
+* неубывающем порядке.
+* Программа должна выполнять ввод текста, вывод его исходного варианта,
+* выполнять необходимые действия и выводить полученный текст.
+*
+* a.matuzov, 3/15/2016 7:02:43 PM
+*/
+#define MIN_STRINGS		10
+#define MAX_STRINGS		100
+#define MIN_WORDS		5
+#define MIN_CHARS		5
+
+char* Rus(const char* text);
+
+void main() {
+
+	//Code goes here
+
+
+}
+
+char bufRus[256];
+
+char* Rus(const char* text)
+{
+	CharToOemA(text, bufRus);
+	return bufRus;
+}
