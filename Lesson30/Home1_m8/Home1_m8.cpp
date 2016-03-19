3dc70be22c2a69fbeeba200eba1293f16e0c17cb
Александр Матузов <a.matuzov@gmail.com>
2016-03-19 16:21:27 +0300

    Home1_m8 update

--- Lesson30/Home1_m8/Home1_m8.cpp
+++ Lesson30/Home1_m8/Home1_m8.cpp
@@ -14,134 +14,312 @@
 #define MIN_STRINGS		10
 #define MIN_WORDS		5
 #define MIN_CHARS		5
-#define MAX_LENGTH		100
 #define HALT			""
+#define DEBUG			0
+#define SEPARATORS		" "
 
-typedef int(*sort_f)(char*, char*);
+typedef int (*sort_f)(char*, char*, void*);
 
-char* Rus(const char* text);
-char** read_strings(char **, int* ct);
-char *read_string(FILE* fp, size_t size);
-void sort(char ** str, int n, sort_f f);
+void show_menu();
+char** read_strings(char**, int*);
+char* read_string(FILE*, size_t);
+char** words_str(char*, int*, char*);
+int check_string(char*);
+void sort(char**, int, sort_f, void*);
+int char_count(char*, char*);
+int word_length(char*, char*);
+int char_count_comp_ASC(char*, char*, void*);
+char* Rus(const char*);
 
-int char_count(char* str);
-int char_count_comp_ASC(char* l, char* r);
-
-char sort_symbol = 'a';
-
-void main() {
-
-	//Code goes here
-	char ** str = NULL;
+void main()
+{
+	char** str = NULL;
 	int count = 0;
 
 	printf(Rus("Вводите строки и они будут отсортированы. Необходимо ввести не меннее %d строк.\n"), MIN_STRINGS);
 	printf(Rus("Для прекращения ввода-<Enter> в начале строки.\n"));
-	str = read_strings(str, &count);
+	do
+	{
+		str = read_strings(str, &count);
+		if (count < MIN_STRINGS)
+			printf(Rus("Строк меньше %d. Вводите дополнительные строки.\n"), MIN_STRINGS);
+	}
+	while (count < MIN_STRINGS);
 
 	sort_f f = &char_count_comp_ASC;
+	printf(Rus("Введите символ: "));
+	char* symbol = read_string(stdin, 1);
 
 	int i;
-	//for (i = 0; i < count; i++)
-	//{
-	//	printf("%s - %d\n", *(str + i), char_count_comp_ASC(*str, *(str+i)));
-	//}
-	//puts("\n");
+	for (i = 0; i < count; ++i)
+	{
+		puts(*(str + i));
+	}
+	puts("\n");
 
-	sort(str, count, f);
-	for (i = 0; i < count; i++)
+	sort(str, count, f, &symbol);
+
+	for (i = 0; i < count; ++i)
 	{
 		puts(*(str + i));
 	}
 
+	for (i = 0; i < count; ++i)
+	{
+		free(*(str + i));
+	}
+	free(str);
 }
 
-char** read_strings(char **str, int* count)
+
+/*
+* Function:  read_strings
+* --------------------
+* Читает строки в массив пока не получит пустую строку
+*
+*  str:		Массив строк
+*  count:	Количество строк в массиве
+*
+*  returns:	Указатель на массив строк
+*/
+char** read_strings(char** str, int* count)
 {
 	char* s;
 	int alloc = *count + 10;
 	str = (char**)realloc(str, sizeof(char*) * alloc);
 
-	while ((s = read_string(stdin, 10)) && strcmp(s, HALT)){
-		if (*count == alloc)
-			str = (char**)realloc(str, sizeof(char*)*(alloc+=10));
-		*(str + *count) = s;
-		++(*count);
+	while ((s = read_string(stdin, 10)) && strcmp(s, HALT))
+	{
+		if (check_string(s))
+		{
+			if (*count == alloc)
+				str = (char**)realloc(str, sizeof(char*) * (alloc += 10));
+			*(str + *count) = s;
+			++(*count);
+		}
 	}
-	return (char**)realloc(str,sizeof(char*) * *count);
+	return (char**)realloc(str, sizeof(char*) * *count);
 }
 
-char* read_string(FILE* fp, size_t size) {
-	char *str;
-	int ch;
+
+/*
+* Function:  read_string
+* --------------------
+* Читает строку с указанного потока
+*
+*  fp:		Поток ввода
+*  count:	Начальный размер строки
+*
+*  returns:	Указатель на прочитанную строку
+*/
+char* read_string(FILE* fp, size_t size)
+{
+	char* str;
+	char ch;
 	size_t len = 0;
-	str = (char*)calloc(size, sizeof(char));
-	while (EOF != (ch = fgetc(fp)) && ch != '\n') {
+	str = (char*)calloc(size + 1, sizeof(char));
+	while (EOF != (ch = fgetc(fp)) && ch != '\n')
+	{
 		str[len++] = ch;
-		if (len == size) {
-			str = (char*)realloc(str, sizeof(char)*(size += 16));
+		if (len == size)
+		{
+			str = (char*)realloc(str, sizeof(char) * (size += 16));
 			if (!str)return str;
 		}
 	}
 	str[len++] = '\0';
 
-	return (char*)realloc(str, sizeof(char)*len);
+	return (char*)realloc(str, sizeof(char) * len);
 }
 
-char bufRus[256];
-char* Rus(const char* text)
-{
-	CharToOemA(text, bufRus);
-	return bufRus;
-}
 
-void sort(char ** str, int n, sort_f f)
+/*
+* Function:  words_str
+* --------------------
+* Возвращает массив указателей на начало слов в строке
+*
+*  str:		Строка
+*  count:	Количество слов
+*  sep:		Строка с разделителями слов
+*
+*  returns:	Указатель на массив указателей на начало слов
+*/
+char** words_str(char* str, int* count, char* sep)
 {
-	static int iter = 1;
-	printf("\nIteration = %d, array:\n", iter++);
-	int i;
-	for (i = 0; i < n; i++)
+	int alloc = *count + 5;
+	char** words = (char**)calloc(alloc, sizeof(char*));
+	int insep = 1;
+	while (*str)
 	{
-		puts(*(str + i));
+		if (strchr(sep, *str))
+		{
+			insep = 1;
+		}
+		else if (insep)
+		{
+			if (*count == alloc)
+				words = (char**)realloc(words, sizeof(char*) * (alloc += 5));
+			*(words + (*count)++) = str;
+			insep = 0;
+		}
+		++str;
 	}
+	return (char**)realloc(words, sizeof(char*) * *count);
+}
 
 
-	int l = 0, r = n - 1;
-	char* m = *(str + n / 2);
+/*
+* Function:  check_string
+* --------------------
+* Проверяет строку на соответствие условиям
+*
+*  string:	Строка
+*
+*  returns:	1 если строка соответствует всем условиям иначе 0
+*/
+int check_string(char* string)
+{
+	int words_count = 0, chars_count = 0;
+	char** words = words_str(string, &words_count, SEPARATORS);
+	if (words_count < MIN_WORDS)
+	{
+		printf(Rus("Слов должно быть не меньше %d. Вы ввели %d. Строка не сохранена, повторите ввод.\n"), MIN_WORDS, words_count);
+		free(words);
+		return 0;
+	}
+	int i;
+	for (i = 0; i < words_count; ++i)
+	{
+		if ((chars_count = word_length(*(words + i), SEPARATORS)) < MIN_CHARS)
+		{
+			printf(Rus("В слове должно быть не менее %d символов. В %d слове вы ввели %d символов. Строка не сохранена, повторите ввод.\n"), MIN_CHARS, i + 1, chars_count);
+			free(words);
+			return 0;
+		}
+	}
+	free(words);
+	return 1;
+}
+
+
+/*
+* Function:  sort
+* --------------------
+* Функция сортировки строк. Способ сортировки определяется передаваемой функцией
+*
+*  str:		Массив строк
+*  n:		Количество строк
+*  f:		Указатель на функцию сравнения двух строк
+*/
+void sort(char** str, int n, sort_f f, void* param)
+{
+	if (DEBUG)
+	{
+		static int iter = 1;
+		printf("\nIteration = %d, array:\n", iter++);
+		int i;
+		for (i = 0; i < n; i++)
+		{
+			puts(*(str + i));
+		}
+	}
+	int i, j = 0, sorted;
 	char* tmp;
 
 	do
 	{
-		while ((*f)(*(str + l), m) < 0) l++;
-		while ((*f)(*(str + r), m) > 0) r--;
-
-		if (l <= r)
+		sorted = 1;
+		for (i = 0; i < n - j - 1; i++)
 		{
-			tmp = *(str + l);
-			*(str + l) = *(str + r);
-			*(str + r) = tmp;
-			l++;
-			r--;
+			if ((*f)(*(str + i), *(str + i + 1), param) < 0)
+			{
+				tmp = *(str + i);
+				*(str + i) = *(str + i + 1);
+				*(str + i + 1) = tmp;
+				sorted = 0;
+			}
 		}
-	} while (l <= r);
-
-
-	if (r > 0) sort(str, r + 1, f);
-	if (n > l) sort(str + l, n - l, f);
+		j++;
+	}
+	while (!sorted);
 }
 
-int char_count(char * str)
+
+/*
+* Function:  char_count
+* --------------------
+* Возвращает количество заданных символов в строке
+*
+*  str:		Строка для подсчета
+*  symbol:	Строка символов для подсчета
+*
+*  returns:	Количество
+*/
+int char_count(char* str, char* symbol)
 {
 	int count = 0;
-	while (*str){
-		if (*str == sort_symbol)
+	while (*str)
+	{
+		if (strchr(symbol, *str))
+			++count;
+		++str;
+	}
+	return count;
+}
+
+
+/*
+* Function:  word_length
+* --------------------
+* Возвращает количество символов в слове до разделителя
+*
+*  str:		Строка для подсчета
+*  sep:		Строка разделителей
+*
+*  returns:	Количество
+*/
+int word_length(char* str, char* sep)
+{
+	int count = 0;
+	while (!strchr(sep, *str) && *str != '\0')
+	{
 		++count;
 		++str;
 	}
 	return count;
 }
 
-int char_count_comp_ASC(char * l, char * r)
+
+/*
+* Function:  char_count_comp_ASC
+* --------------------
+* Сравнивает две строки по количеству символов и возвращает результат в виде целого числа.
+* Если в первом операнде количество заданных символов меньше результат будет отрицательным
+*
+*  l:		Первая строка для сравнения
+*  l:		Вторая строка для сравнения
+*
+*  returns:	Разницу в количестве символов
+*/
+int char_count_comp_ASC(char* l, char* r, void* param)
 {
-	return char_count(l) - char_count(r);
+	return char_count(l, (char*)param) - char_count(r, (char*)param);
+}
+
+
+char bufRus[256];
+
+/*
+* Function:  Rus
+* --------------------
+* Переводит строку в кодировку консоли
+*
+*  text:	исходная строка
+*
+*  returns: Строка в кодировке консоли
+*/
+char* Rus(const char* text)
+{
+	CharToOemA(text, bufRus);
+	return bufRus;
 }
