71500974e3192dec25b577db75ece76d3740de87
Александр Матузов <a.matuzov@gmail.com>
2016-03-29 20:19:01 +0300

    Looks like good place for library

---
+++ Library/io.cpp
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include "strings.h"
+#include "io.h"
+
+
+void flush_stream(FILE* fp)
+{
+	fseek(fp, 0, SEEK_END);
+}
+
+
+char* read_string(FILE* fp)
+{
+	char* str, ch;
+	size_t len = 0, alloc = 16;
+
+	str = (char*)calloc(alloc, sizeof(char));
+
+	while (EOF != (ch = fgetc(fp)) && ch != '\n')
+	{
+		str[len++] = ch;
+		if (len == alloc)
+		{
+			str = (char*)realloc(str, sizeof(char) * (alloc += 16));
+			if (!str)return str;
+		}
+	}
+	str[len++] = '\0';
+
+	return (char*)realloc(str, sizeof(char) * len);
+}
+
+
+char** read_strings(FILE* fp, char** str, size_t* count)
+{
+	char* s;
+	int alloc = *count + 10;
+	str = (char**)realloc(str, sizeof(char*) * alloc);
+
+	while (((s = read_string(fp))) && str_cmp(s, HALT))
+	{
+		if (*count == alloc)
+			str = (char**)realloc(str, sizeof(char*) * (alloc += 10));
+		*(str + *count) = s;
+		++(*count);
+	}
+	return (char**)realloc(str, sizeof(char*) * *count);
+}
+
+
+__int64 read_int(FILE* fp)
+{
+	__int64 n = 0;
+	__int8 result;
+	char ch;
+	do
+	{
+		result = fscanf(fp, "%lld", &n);
+		if (result == EOF)
+		{
+			fprintf(stderr, "Unexpected EOF\n");
+			exit(74);
+		}
+		if (result == 0)
+		{
+			do
+			{
+				ch = fgetc(fp);
+			} while (ch <= '0' && ch >= '9');
+			fprintf(stderr, "Incorrect symbol\n");
+		}
+	} while (!result);
+
+	flush_stream(fp);
+
+	return n;
+}
+
+char read_char(FILE* fp)
+{
+	char ch = fgetc(fp);
+
+	flush_stream(fp);
+
+	return ch;
+}
+
+void print_strings(char** str, size_t count)
+{
+	size_t i;
+	for (i = 0; i < count; ++i)
+	{
+		puts(*(str + i));
+	}
+}
---
+++ Library/utils.cpp
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <malloc.h>
+
+size_t num_char_count(__int64 num)
+{
+	size_t count = 1;
+	if (num)
+	{
+		count = (size_t)(floor(log10(abs(num))) + 1);
+		if (num < 0)
+			++count;
+	}
+	return count;
+}
+
+__int8 is_digit(char ch)
+{
+	return (ch >= '0' && ch <= '9');
+}
+
+void* free_ptr_ptr(void **ptr, size_t count)
+{
+	size_t i;
+	for (i = 0; i < count; ++i)
+	{
+		free(*(ptr + i));
+	}
+	free(ptr);
+	return NULL;
+}
