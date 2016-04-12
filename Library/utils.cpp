140fc6e5294cd0cc56ac0de5d531329e4414f67f
Александр Матузов <a.matuzov@gmail.com>
2016-04-12 23:28:47 +0300

    Library fixes

--- Library/utils.cpp
+++ Library/utils.cpp
@@ -1,17 +1,17 @@
-#include <math.h>
 #include <malloc.h>
 
 size_t num_char_count(__int64 num)
 {
-	size_t count = 1;
-	if (num)
-	{
-		count = (size_t)(floorl(log10((double)llabs(num))) + 1);
-		if (num < 0)
-			++count;
-	}
+	size_t count = 0;
+	if (num < 0)
+		++count;
+	do {
+		++count;
+		num /= 10;
+	} while (num);
 	return count;
 }
+
 
 
 __int8 is_digit(char ch)
@@ -35,4 +35,26 @@ void* free_ptr_ptr(void **ptr, size_t count)
 	}
 	free(ptr);
 	return NULL;
+}
+
+__int64 pow(__int64 base, __int64 exp)
+{
+	if (!exp) {
+		return 0;
+	}
+
+	__int64 result = 1;
+	while (exp)
+	{
+		if (exp & 1)
+			result *= base;
+		exp >>= 1;
+		base *= base;
+	}
+
+	return result;
+}
+
+__int64 _llabs(__int64 x) {
+	return x < 0 ? x * -1 : x;
 }
